15ab9fc5614fab58ae00daca8e94eb64bf81c0e1
Shacklebottom <korzinekt@gmail.com>
2026-06-29 17:29:07 -0500

    added the TestHarness and its project references

---
+++ TestHarness/TSEntry/Program.cs
@@ -0,0 +1,13 @@
+﻿using LengthUnits;
+
+
+void Turtles()
+{
+    var x = InchesReferenceData.InchesCollection;
+    foreach ( var item in x)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+Turtles();
