d8950b5a26045b1d2b92804103f576c7fdace97a
Shacklebottom <korzinekt@gmail.com>
2026-06-29 15:22:30 -0500

    added ListExtension, an extension method for 'true' shuffling lists

---
+++ RandomizerUtil/ListExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+namespace RandomizerUtil
+{
+    public static class ListExtensions
+    {
+        public static void Shuffle<T>(this List<T> collection)
+        {
+            for (int i = collection.Count - 1; i > 0; i--)
+            {
+                var j = RandomHelper.GetRandom(i + 1);
+
+                (collection[i], collection[j]) = (collection[j], collection[i]);
+            }
+        }
+    }
+}
