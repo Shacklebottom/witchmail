6f13db2435ab21f452fc6766c9f56874da214862
Shacklebottom <korzinekt@gmail.com>
2026-06-29 14:29:36 -0500

    added FeetReferenceData, removed a mysterious duplicate from InchesReferenceData lmao

---
+++ FeetReferenceData.cs
@@ -0,0 +1,14 @@
+﻿
+namespace LengthUnits
+{
+    public class FeetReferenceData
+    {
+        public static readonly List<string> FeetCollection = 
+        [
+            "ft",
+            "foot",
+            "feet",
+            "'",
+        ];
+    }
+}
--- InchesReferenceData.cs
+++ InchesReferenceData.cs
@@ -9,7 +9,6 @@ namespace LengthUnits
             "inch",
             "inches",
             "\"",
-            "\"",
         ];
     }
 }
