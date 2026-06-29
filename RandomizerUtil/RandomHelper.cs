6ce4732b80043d0f644b440a11e2626a1cb85af1
Shacklebottom <korzinekt@gmail.com>
2026-06-29 15:07:53 -0500

    added RandomizerUtil and RandomHelper proper

---
+++ RandomizerUtil/RandomHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace RandomizerUtil
+{
+    public class RandomHelper
+    {
+        private static readonly Random _random = new();
+
+        public static int GetRandom(int len)
+        {
+            return _random.Next(len);
+        }
+    }
+}
